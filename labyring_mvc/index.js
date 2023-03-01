8e3958f2b8ecbdf69f27fc0f955531133d44c80b
Thomasgetacademy <122350692+Thomasgetacademy@users.noreply.github.com>
2023-03-01 09:23:56 +0100

    Labyrint update

--- labyring_mvc/index.js
+++ labyring_mvc/index.js
@@ -1,191 +1,113 @@
-// Model
+/* --------------------------------------------------- Model ------------------------------------------------------------*/
+
+let mainDiv = document.getElementById('app');
 
 let mazeModel = {
     size: 2,
-    rows: [
-        [
-            { isHigh: false, isWide: false, isOpen: false },
-            { isHigh: false, isWide: true, isOpen: false },
-            { isHigh: false, isWide: false, isOpen: false },
-            { isHigh: false, isWide: true, isOpen: false },
-            { isHigh: false, isWide: false, isOpen: false },
-        ],
-        [
-            { isHigh: true, isWide: false, isOpen: false },
-            { isHigh: true, isWide: true },
-            { isHigh: true, isWide: false, isOpen: false },
-            { isHigh: true, isWide: true },
-            { isHigh: true, isWide: false, isOpen: false },
-        ],
-        [
-            { isHigh: false, isWide: false, isOpen: false },
-            { isHigh: false, isWide: true, isOpen: false },
-            { isHigh: false, isWide: false, isOpen: false },
-            { isHigh: false, isWide: true, isOpen: false },
-            { isHigh: false, isWide: false, isOpen: false },
-        ],
-        [
-            { isHigh: true, isWide: false, isOpen: false },
-            { isHigh: true, isWide: true },
-            { isHigh: true, isWide: false, isOpen: false },
-            { isHigh: true, isWide: true },
-            { isHigh: true, isWide: false, isOpen: false },
-        ],
-        [
-            { isHigh: false, isWide: false, isOpen: false },
-            { isHigh: false, isWide: true, isOpen: false },
-            { isHigh: false, isWide: false, isOpen: false },
-            { isHigh: false, isWide: true, isOpen: false },
-            { isHigh: false, isWide: false, isOpen: false },
-        ],
-
-    ]
+    rows: [],
 };
 
-let size = 2;
-
-init(size);
-let matrixView = document.getElementById('app');
-let mazeRow = mazeModel.rows;
-
-// View
-
+updateModel(mazeModel.size);
 updateView();
+
+/* --------------------------------------------------- View -------------------------------------------------------------*/
+
 function updateView() {
-    matrixView.innerHTML = '';
-    for (let rowCounter = 0; rowCounter < mazeModel.rows.length; rowCounter++) {
-        let viewRow = matrixView.insertRow();
-        let modelRow = mazeModel.rows[rowCounter];
-        for (let cellCounter = 0; cellCounter < modelRow.cells.length; cellCounter++) {
-            let viewCell = viewRow.insertCell();
-            let modelCell = modelRow.cells[cellCounter];
-            if (modelCell.isSide) {
-                let test;
-            } else {
-                let test1;
-            }
-        }
-    }
-
-    html = /* HTML */`
-    <input onchange="mazeModel.size = this.value, updateView()" value=${mazeModel.size} type="range" min="2" max="10">
-    
-    ${createMaze()}
+    mainDiv.innerHTML = `
+    <table>
+        ${mazeModel.rows.map((row, rowIndex) => `
+        <tr>
+            ${row.map((col, colIndex) => `
+            <td class="${setClasses(col)}" onclick="toggle(${rowIndex}, ${colIndex})">
+            `).join('')}
+        </tr>
+        `).join('')}
+    </table>
 `;
-    matrixView.innerHTML = html;
 }
 
-function createMaze() {
-    // console.log(mazeModel.size);
-    let mazeReturn = '';
-    // constructMaze();
-    for (let a = 0; a < mazeRow.length; a++) { /* a = Array Index */
-        // console.log(`a ===    ${a}`);
-        mazeReturn += '<table><tr>';
-        for (let i = 0; i < mazeRow[a].length; i++) {
-            // console.log(`i ===    ${mazeRow[a].length}`);
-            let classes = '';
-            if (mazeRow[a][i].isHigh && mazeRow[a][i].isWide/*  && !mazeRow[a][i].isOpen */) {
-                classes += 'room ';
-                mazeReturn += `<td class="${classes}"></td>`;
-            } else {
-                mazeRow[a][i].isHigh ? classes += 'high ' : classes += 'low ';
-                mazeRow[a][i].isWide ? classes += 'wide ' : classes += 'small ';
-                mazeRow[a][i].isOpen ? classes += 'noWall ' : classes += 'wall ';
-                classes ? mazeReturn += `<td class="${classes}"></td>` :
-                    mazeReturn += `<td></td>`;
-            }
-        }
-        mazeReturn += '</table></tr>';
-    }
-    return mazeReturn;
-}
+/* --------------------------------------------------- CONTROLLER -------------------------------------------------------*/
 
-function init(size) {
-    mazeModel = {};
+function updateModel(newSize) {
     mazeModel.rows = [];
 
-    for (let rowCounter = 0; rowCounter < size; rowCounter++) {
-        let newRow = {};
-        newRow.cells = [];
-        for (let cellCounter = 0; cellCounter < size; cellCounter++) {
-            let newCell = {};
-            newCell.isSide = false;
-            newCell.isButtom = false;
-            newRow.cells.push(newCell);
-        }
-        // console.log(newRow)
-        // console.log(mazeModel.rows)
-        mazeModel.rows.push(newRow);
+    for (let rowCounter = 0; rowCounter < newSize; rowCounter++) { /* will run 5 times to create every object inside a row */
+
+        mazeModel.rows.push(newCellFunc(newSize, false)); /* isHigh false */
+        mazeModel.rows.push(newCellFunc(newSize, true)); /* isHigh true */
     }
+    mazeModel.rows.push(newCellFunc(newSize, false)); /* isHigh false */
+
+    closeRow(0); /* Closes top of the maze */
+    closeRow(mazeModel.rows.length - 1); /* Closes bottom of the maze */
+    closeColumn(0); /* Closes left side of the maze */
+    closeColumn(mazeModel.rows.length - 1); /* Closes right side of the maze */
+    setEntry();
+    setExit();
 }
 
+function newCellFunc(newSize, isHigh) {
+    const row = [];
+    for (let colIndex = 0; colIndex < newSize; colIndex++) {
+        row.push(createCell(isHigh, false)); /* isWide false */
+        row.push(createCell(isHigh, true)); /* isWide true */
+    }
+    row.push(createCell(isHigh, false)); /* isWide false */
+    return row;
+}
 
+function createCell(isHigh, isWide) {
+    if (isHigh === isWide) return { isHigh, isWide };
+    const isOpen = Math.random() < 0.5 ? true : false;
+    return { isHigh, isWide, isOpen };
+}
 
+function closeRow(index) {
+    const row = mazeModel.rows[index];
+    for (let cell of row) {
+        cell.isOpen = false;
+    }
+}
+function closeColumn(index) {
+    for (let row of mazeModel.rows)
+        row[index].isOpen = false;
+}
 
+function setEntry() {
+    let randomIndex = length();
+    while (randomIndex === mazeModel.rows.length) {
+        randomIndex = length();
+    }
+    mazeModel.rows[randomIndex][0].isOpen = true;
+}
 
+function setExit() {
+    let randomIndex = length();
+    while (randomIndex === mazeModel.rows.length) {
+        randomIndex = length();
+    }
+    mazeModel.rows[randomIndex][mazeModel.rows.length - 1].isOpen = true;
+}
 
-// function constructMaze() {
-//     for (i = 0; i < mazeRow.length; i++) {
-//         mazeRow[i] = [];
-//     }
-//     mazeRow[0] = [];
-//     for (i = 0; i < mazeModel.size - 3; i++) {
-//         /* BUNN */
-//         for (i = 0; i < mazeModel.size - 3; i++) {
-//             mazeRow.push([
-//                 { isHigh: true, isWide: false, isOpen: false },
-//                 { isHigh: true, isWide: true },
-//                 { isHigh: true, isWide: false, isOpen: false },
-//             ]);
-//             mazeRow[mazeRow.length - 1].push([
-//                 { isHigh: false, isWide: false, isOpen: false },
-//                 { isHigh: false, isWide: true, isOpen: false },
-//                 { isHigh: false, isWide: false, isOpen: false },
-//             ]);
-//         }
-//     }
-    // mazeRow.push([
-    //     { isHigh: true, isWide: false, isOpen: false },
-    //     { isHigh: true, isWide: true },
-    //     { isHigh: true, isWide: false, isOpen: false },
-    // { isHigh: true, isWide: true },
-    // { isHigh: true, isWide: false, isOpen: false },
-    // { isHigh: true, isWide: true },
-    // { isHigh: true, isWide: false, isOpen: false },
-    // ]);
-    // mazeRow.push([
-    // { isHigh: false, isWide: false, isOpen: false },
-    // { isHigh: false, isWide: true, isOpen: false },
-    // { isHigh: false, isWide: false, isOpen: false },
-    // { isHigh: false, isWide: true, isOpen: false },
-    //     { isHigh: false, isWide: false, isOpen: false },
-    //     { isHigh: false, isWide: true, isOpen: false },
-    //     { isHigh: false, isWide: false, isOpen: false },
-    // ]);
+/* Creates a variable that  */
+function length() {
+    let returnData = Math.floor(Math.random() * (mazeModel.rows.length / 2)) * 2 + 1;
+    return returnData;
+}
 
-/* Side */
-    // mazeRow[0].push(
-    //     { isHigh: false, isWide: true, isOpen: false },
-    //     { isHigh: false, isWide: false, isOpen: false },
-    // );
-    //         mazeRow[1].push(
-    //             { isHigh: true, isWide: true },
-    //             { isHigh: true, isWide: false, isOpen: false },
-    //         );
-    //         mazeRow[2].push(
-    //             { isHigh: false, isWide: true, isOpen: false },
-    //             { isHigh: false, isWide: false, isOpen: false },
-    //         );
-    //         mazeRow[3].push(
-    //             { isHigh: true, isWide: true },
-    //             { isHigh: true, isWide: false, isOpen: false },
-    //         );
-    //         mazeRow[4].push(
-    //             { isHigh: false, isWide: true, isOpen: false },
-    //             { isHigh: false, isWide: false, isOpen: false },
-    //         );
+function toggle(rowIndex, colIndex) {
+    if (colIndex !== 0 && colIndex !== mazeModel.rows.length - 1 && rowIndex !== 0 && rowIndex !== mazeModel.rows.length - 1) {
+        let mazePath = mazeModel.rows[rowIndex][colIndex];
+        if (mazePath.isOpen === undefined) return;
+        mazePath.isOpen = !mazePath.isOpen;
+    }
+    updateView();
+}
 
-
-
-    // { isHigh: true, isWide: true }  === Blank square,
+function setClasses(cell) {
+    if (cell.isHigh && cell.isWide) return 'room';
+    const setHigh = cell.isHigh ? 'high ' : 'low ';
+    const setWide = cell.isWide ? 'wide ' : 'small ';
+    const setOpen = cell.isOpen ? 'noWall ' : 'wall ';
+    return setHigh + setWide + setOpen;
+}
